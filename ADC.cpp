53561b35a140d70c536016fdd90c36b8728ba23e
jessifedalto <142901908+jessifedalto@users.noreply.github.com>
2023-11-22 14:09:50 -0300

    Utilizing potentiometer

--- ADC.cpp
+++ ADC.cpp
@@ -1,16 +1,18 @@
 const uint8_t PIN_INPUT = 26;
 
-/*------Constants-----*/
 const uint16_t ADC_MAX = 4095;
-const float VCC = 3.3;
+const float VCC = 3.3f;
 
-void setup() 
+void setup()
 {
   pinMode(PIN_INPUT, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() 
 {
-  int input = analogRead(PIN_INPUT);
-  float tensao = static_cast<float>(input) * VCC / ADC_MAX;  
+  uint16_t input = analogRead(PIN_INPUT);
+  float voltage = input *  VCC / ADC_MAX;
+
+  Serial.println(voltage);
 }
